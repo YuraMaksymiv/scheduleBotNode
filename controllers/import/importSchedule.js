8547d4f08417ba8a07eee3cad7cb1aaa69732d2d
yura <yu.maksymiv@gmail.com>
2020-04-18 21:38:44 +0300

    import schedule

--- controllers/import/importSchedule.js
+++ controllers/import/importSchedule.js
@@ -11,72 +11,101 @@ module.exports = async (req, res) => {
         let workbook = excelToJson({
             sourceFile: 'files/rozklad_idtd.xls'//file should be get from request
         });
-        workbook = workbook["КН, ІСТ"];
-        // here should be forin for all groups
-        let firstRow = workbook[0];
 
-        //form table to save in DB
-        for (const key in firstRow) {
-            table[key] = {groupName: firstRow[key]};
-            table[key].days = [];
-            for (let i = 0; i < 5; i++) {
-                let lessonData = [];
-                for (let j = 0; j < 5; j++) {
-                    let number = j+1;
-                    lessonData.push({
-                        numberOfLesson: '' + number,
-                        time: times[i],
-                        nameOfLesson: ["-/-", "-/-"]
-                    })
-                };
-                table[key].days.push({
-                    day: dayNames[i],
-                    lesson: lessonData
-                });
+        for (const workBookKey in workbook) {
+            let newWorkbook = workbook[workBookKey];
+            let firstRow = newWorkbook[0];
+
+            //form table to save in DB
+            for (const key in firstRow) {
+                table[key] = {groupName: firstRow[key]};
+                table[key].days = [];
+                for (let i = 0; i < 5; i++) {
+                    let lessonData = [];
+                    for (let j = 0; j < 5; j++) {
+                        let number = j+1;
+                        lessonData.push({
+                            numberOfLesson: '' + number,
+                            time: times[i],
+                            nameOfLesson: ["-/-", "-/-"]
+                        })
+                    };
+                    table[key].days.push({
+                        day: dayNames[i],
+                        lesson: lessonData
+                    });
+                }
             }
-        }
 
-        delete table["A"];//should delete first and last
-        delete table["AP"];
-
-        // let newTable = [];
-        // for (const key in table) {
-        //     newTable.push(table[key]);
-        // }
-        // table = newTable
-
-        for (let i = 1; i < workbook.length; i++) {
             let count = 1, day = 0, lesson = 0;
-            let currentRow = workbook[i];
+            for (let i = 1; i < newWorkbook.length; i+=2) {
+                let currentRow = newWorkbook[i];
+                let nextRow = newWorkbook[i+1];
 
-            console.log(currentRow);
+                for (const rowKey in currentRow) {
+                    if(table[rowKey] && table[rowKey].days) {
+                        table[rowKey].days[day].lesson[lesson].nameOfLesson[0] = currentRow[rowKey];
+                        if(!nextRow[rowKey]) table[rowKey].days[day].lesson[lesson].nameOfLesson[1] = currentRow[rowKey];
+                        else if(nextRow[rowKey]) table[rowKey].days[day].lesson[lesson].nameOfLesson[1] = nextRow[rowKey];
+                    }
 
-            for (const rowKey in currentRow) {
+                }
 
+                if(count === 5) { //check day
+                    count = 1;
+                    day++;
+                    lesson = 0;
+                } else {
+                    count++;
+                    lesson++
+                }
             }
 
-            break
+            let newTable = [];
+            for (const key in table) {
+                newTable.push(table[key]);
+            }
+            newTable.shift();
+            newTable.pop();
+            table = newTable;
 
-            if(i % 2 === 0) lesson++; //check number of lesson
-            if(count === 10) { //check day
-                count = 1;
-                // i++;
-                day++;
-                lesson = 0;
+            for (let i = 0; i < table.length; i++) {
+                let currentGroup = table[i];
+                let name = currentGroup.groupName.split("/");
+                if(name[1] && name[1] == "1" || name[1] == "1м") {
+                    let otherGroups = table.filter(g => {
+                       if(g.groupName !== currentGroup.groupName) {
+                           let n = g.groupName.split('/');
+                           if(n[0] == name[0]) return g
+                       }
+                    })
+                    if(otherGroups.length) {
+                        for (let j = 0; j < otherGroups.length; j++) {
+                            let currOtherGroup = otherGroups[j];
+                            for (let k = 0; k < 5; k++) {
+                                for (let l = 0; l < 5; l++) {
+                                    if(currOtherGroup.days[k].lesson[l].nameOfLesson[0] === "-/-") {
+                                        currOtherGroup.days[k].lesson[l].nameOfLesson[0] = currentGroup.days[k].lesson[l].nameOfLesson[0]
+                                    }
+                                    if(currOtherGroup.days[k].lesson[l].nameOfLesson[1] === "-/-") {
+                                        currOtherGroup.days[k].lesson[l].nameOfLesson[1] = currentGroup.days[k].lesson[l].nameOfLesson[1]
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (const key in table) {
+                await req.mongoConnection.updateSchedule(table[key])
             }
         }
-        
-        
-        
-        
 
-        // for (const key in table) {
-        //     await req.mongoConnection.updateSchedule(table[key])
-        // }
 
         return res.json({
             code: 200,
-            data: table
+            data: true
         });
     } catch (e) {
         console.log(e);
